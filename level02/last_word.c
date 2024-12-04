ebed34cb7a97675eca6917485e50be7107d744ff
Killua <106412328+Killua0615@users.noreply.github.com>
2024-12-04 18:06:04 +0900

    commit

--- level02/last_word.c
+++ level02/last_word.c
@@ -1,33 +1,5 @@
 #include <unistd.h>
 
-int main(int ac, char **av)
-{
-  int i = 0;
-
-  if (ac == 2)
-  {
-    while (av[1][i])
-      i++;
-    i--;
-    while (i >= 0 && (av[1][i] == ' ' || av[1][i] == '\t'))
-      i--;
-    int end = i;
-    while (i >= 0 && av[1][i] != ' ' && av[1][i] != '\t')
-      i--;
-    i++;
-    while (i <= end)
-    {
-      write(1, &av[1][i], 1);
-      i++;
-    }
-  }
-  write(1, "\n", 1);
-  return 0;
-}
-
-//-------------------------------------------//
-#include <unistd.h>
-
 void	last_word(char *str)
 {
 	int j = 0;
